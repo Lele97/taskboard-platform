ca4b30271b83e5b4d6aaff9da034ad277eb1b95e
Gabriele Grandinetti <gabriele.grandinetti@hotmail.com>
2026-03-20 17:31:23 +0100

    first commit

---
+++ taskboard-platform/auth-service/auth-service/src/main/java/com/local/taskboard/AuthServiceApplication.java
@@ -0,0 +1,11 @@
+package com.local.taskboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
