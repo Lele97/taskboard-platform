a3e132b39d23aa26958d6c4ef31400511d453ec9
Lele97 <gabriele.grandinetti@hotmail.com>
2026-04-17 19:33:48 +0200

    refactor: improve AnalyticsGatewayController response handling

--- gateway-service/src/main/java/com/local/taskboard/controller/AnalyticsGatewayController.java
+++ gateway-service/src/main/java/com/local/taskboard/controller/AnalyticsGatewayController.java
@@ -1,10 +1,10 @@
 package com.local.taskboard.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 
@@ -16,19 +16,25 @@ public class AnalyticsGatewayController {
     private final WebClient analyticsWebClient;
 
     @GetMapping
-    public Mono<ResponseEntity<Flux<String>>> getAnalytics(@RequestHeader(value = "Authorization", required = true) String authHeader) {
+    public Mono<ResponseEntity<String>> getAnalytics(@RequestHeader(value = "Authorization") String authHeader) {
         return analyticsWebClient.get().uri("/api/analytics")
                 .header("Authorization", authHeader != null ? authHeader : "")
                 .retrieve()
-                .toEntityFlux(String.class);
+                .bodyToMono(String.class)
+                .map(body -> ResponseEntity.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(body));
     }
 
     @GetMapping("/{boardId}")
-    public Mono<ResponseEntity<String>> getAnalyticsByBoardId(@PathVariable String boardId, @RequestHeader(value = "Authorization", required = true) String authHeader) {
+    public Mono<ResponseEntity<String>> getAnalyticsByBoardId(@PathVariable String boardId, @RequestHeader(value = "Authorization") String authHeader) {
         return analyticsWebClient.get().uri(uriBuilder -> uriBuilder
                         .path("/api/analytics/{boardId}")
                         .build(boardId)).header("Authorization", authHeader != null ? authHeader : "")
                 .retrieve()
-                .toEntity(String.class);
+                .bodyToMono(String.class)
+                .map(body -> ResponseEntity.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(body));
     }
 }
